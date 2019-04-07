66a80d38821f64ce5fe37406297544e13eb5ccba
karbawona <37221123+karbawona@users.noreply.github.com>
2019-04-07 22:18:08 +0200

    Initial Commit

---
+++ Menu-Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Menu-Desktop
+//
+//  Created by Ola on 07/04/2019.
+//  Copyright © 2019 Aleksandra Klein. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
